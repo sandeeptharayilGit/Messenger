9f5f16925d68dc561c144f2cff508864d09d1f31
Sandeep Tharayil <sandeeptharayil@outlook.com>
2013-11-20 16:37:54 +0530

    commit for today
    user login
    message for a each user
    offline data

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ var app = angular.module('myApp', []);
 // is instantiated only once, and thus acts as a singleton
 // for the scope of the application.
 app.factory('socket', function ($rootScope) {
-	var socket = io.connect('http://localhost:2000');
+	var socket = '';
 	return {
+		init: function(sock){
+		socket=sock;
+		},
 		on: function (eventName, callback) {
 			socket.on(eventName, function () {
 				var args = arguments;
@@ -31,14 +34,32 @@ app.factory('socket', function ($rootScope) {
 function MainCtrl($scope, socket) {
 	$scope.message = '';
 	$scope.messages = [];
-	// When we see a new msg event from the server
-	socket.on('new:msg', function (message) {
-		$scope.messages.push(message);
-	});
+	$scope.showMessenger=false;
+	$scope.userName='';
+	$scope.toUser='';
+	
 	// Tell the server there is a new message
 	$scope.broadcast = function() {
-		socket.emit('broadcast:msg', {message: $scope.message});
-		$scope.messages.push($scope.message);
-		$scope.message = '';
+		if(!angular.equals($scope.toUser,'') && !angular.equals($scope.userName,'') && !angular.equals($scope.message,'')){
+			socket.emit('broadcast:msg', {message: $scope.message,to:$scope.toUser,from:$scope.userName});
+			$scope.messages.push("Me: "+$scope.message);
+			$scope.message = '';
+		}
 	};
+	
+	$scope.login=function(){
+		if(!angular.equals($scope.userName,'')){
+			socket.init(io.connect('http://localhost:2000'));
+			socket.emit('user:add', {userName: $scope.userName});
+		
+			$scope.showMessenger=true;
+			
+			// When we see a new msg event from the server
+			socket.on($scope.userName, function (data) {
+				for(var i=0;i<data.length;i++){
+					$scope.messages.push(data[i].from+": "+data[i].message);
+				}
+			});
+		}
+	}
 }
--- appNode.js
+++ appNode.js
@@ -2,19 +2,51 @@ var app = require('express')(),
  server = require('http').createServer(app),
  io = require('socket.io').listen(server);
  server.listen(2000);
+ var users= new Array();
+ var offlineData= new Array();
  
-app.get('/', function (req, res) {
+app.get('/myMessenger', function (req, res) {
 	res.sendfile(__dirname + '/index.html');
 });
 app.get('/app.js', function(req, res) {
 	res.sendfile(__dirname + '/app.js');
 });
 io.sockets.on('connection', function (socket) {
-console.log('started')
-	socket.emit('new:msg', 'Welcome to AnonBoard');
+	console.log('started')
+	
 	socket.on('broadcast:msg', function(data) {
-	console.log(data.message);
+	console.log("Message from: "+data.from+", to: "+data.to+", message:"+data.message);
+	
+		if(users.indexOf(data.to)>0){
+			socket.broadcast.emit(data.to, {message:data.message,from:data.from});
+		}else{
+		console.log('User '+data.to+' is not logged in');
+			var offdata= new Array();
+			if(offlineData[data.to] ==undefined){
+			
+				offdata.push({from:data.from,message:data.message});
+				offlineData[data.to]=offdata;
+			}else {
+			
+				offdata=offlineData[data.to];
+				if(offdata!=undefined){
+					offdata.push({from:data.from,message:data.message});
+					offlineData[data.to]=offdata
+				}
+			}
+		}
+	});
+	socket.on('user:add', function(data) {
+		users.push(data.userName);
+		console.log(data.userName +" logged in");
+		console.log("Logged in users Count:-"+ users.length);
+		
 		// Tell all the other clients (except self) about the new message
-		socket.broadcast.emit('new:msg', data.message);
+		socket.emit(data.userName,[{ message:'Welcome to WhiteBoard',from:''}]);
+		if(offlineData[data.userName] !=undefined){
+			console.log("before splicing "+offlineData[data.userName])+" from index:"+offlineData.indexOf(data.userName);
+			socket.emit(data.userName, offlineData.splice(offlineData.indexOf(data.userName),1)[0]);
+			console.log("after splicing "+offlineData[data.userName]);
+		}
 	});
 });
