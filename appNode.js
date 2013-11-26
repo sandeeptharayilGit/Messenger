7a8d91a3221271224bc378cd5a52d494b7635e24
Sandeep Tharayil <sandeeptharayil@outlook.com>
2013-11-26 18:25:35 +0530

    added mongodb chagnes

--- appNode.js
+++ appNode.js
@@ -4,6 +4,42 @@ var app = require('express')(),
  server.listen(2000);
  var users= new Array();
  var offlineData= new Array();
+ var clients = {};
+ var mongoose = require('mongoose');
+ var mongo_log = "MongoDB : "
+  var socket_log = "MongoDB : "
+ var mongoose = require('mongoose');
+ var OfflineData=null,OnlineData=null;
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Error in connecting to mongolab'));
+db.once('open', function() {
+console.log(mongo_log+" DB connected");
+	 var messageSchema = new mongoose.Schema({
+		to: String,
+		from: String,
+		message:String,
+		date: Date
+	});
+	messageSchema.methods.speak = function () {
+	  var str = "@from="+this.from
+				+"@to="+this.to
+				+"@message"+this.message
+				+"@time"+this.date;
+	  console.log(mongo_log+str);
+	}
+	messageSchema.statics.getMessages = function(user,callback) {
+		return this.find({ to: user }, callback);
+	};
+
+	OfflineData=mongoose.model('OFFLINE_DB', messageSchema);
+	OnlineData=mongoose.model('ONLINE_DB', messageSchema);
+	
+
+});
+
+mongoose.connect('mongodb://deegouser:deegouser@ds053708.mongolab.com:53708/deego');
+ 
  
 app.get('/myMessenger', function (req, res) {
 	res.sendfile(__dirname + '/index.html');
@@ -12,30 +48,54 @@ app.get('/app.js', function(req, res) {
 	res.sendfile(__dirname + '/app.js');
 });
 io.sockets.on('connection', function (socket) {
-	console.log('started')
+	console.log(socket_log+'started')
 	
 	socket.on('broadcast:msg', function(data) {
 	
-	console.log("Message from: "+data.from+", to: "+data.to+", message:"+data.message);
-	console.log(users);
-		if(users.indexOf(data.to)>=0){
-			socket.broadcast.emit(data.to.toLowerCase(), [{message:data.message,from:data.from}]);
-		}else{
-		console.log('User '+data.to+' is not logged in');
+	console.log(socket_log+"Message from: "+data.from+", to: "+data.to+", message:"+data.message);
+	//console.log(socket_log+users);
+		// if(users.indexOf(data.to)>=0){
+			// socket.broadcast.emit(data.to.toLowerCase(), [{message:data.message,from:data.from}]);
+		// }
+		var messageData = {
+		  to: data.to
+		, from: data.from
+		, message: data.message
+		, date: Date.now()
+		};
 		
+		
+		if (clients[data.to]){
+		  io.sockets.socket(clients[data.to].socket).emit(data.to.toLowerCase(), [{message:data.message,from:data.from}]);
+		  
+		  var onlineData= new OnlineData(messageData);
+		  onlineData.save(function(err, onlineData) {
+			  if (err) return console.error(err);
+			  console.dir(mongo_log+onlineData);
+			});
+		  
+		} else{
+		console.log(socket_log+'User '+data.to+' is not logged in');
+		
+		var offlineDBData= new OfflineData(offlineDBData);
+		  offlineDBData.save(function(err, offlineDBData) {
+			  if (err) return console.error(err);
+			  console.dir(offlineDBData);
+			});
+			
 			var offdata= new Array();
 			if(offlineData[data.to] ==undefined){
-				console.log('offlineData[data.to]='+offlineData[data.to]);
+				console.log(socket_log+'offlineData[data.to]='+offlineData[data.to]);
 				offdata.push({from:data.from,message:data.message});
 				offlineData[data.to]=offdata;
 			}else {
-			console.log('offlineData[data.to]='+offlineData[data.to]);
+			console.log(socket_log+'offlineData[data.to]='+offlineData[data.to]);
 				offdata=offlineData[data.to];
 				if(offdata!=undefined){
 					offdata.push({from:data.from,message:data.message});
 					offlineData[data.to]=offdata
 				}
-				console.log('offlineData[data.to]='+offlineData[data.to]);
+				console.log(socket_log+'offlineData[data.to]='+offlineData[data.to]);
 			}
 		}
 	});
@@ -44,13 +104,17 @@ io.sockets.on('connection', function (socket) {
 		if(users.indexOf(data.userName)<0){
 			users.push(data.userName);
 		}
-		console.log(data.userName +" logged in");
-		console.log("Logged in users Count: "+ users.length);
+		clients[data.username] = {
+		  "socket": socket.id
+		};
+		
+		console.log(socket_log+data.userName +" logged in");
+		console.log(socket_log+"Logged in users Count: "+ users.length);
 		socket.emit(data.userName,[{ message:'Welcome to myMessenger',from:''}]);
 		if(offlineData[data.userName] !=undefined){
-			console.log("before splicing "+offlineData[data.userName]+" from index: "+offlineData.indexOf(data.userName));
+			console.log(socket_log+"before splicing "+offlineData[data.userName]+" from index: "+offlineData.indexOf(data.userName));
 			socket.emit(data.userName.toLowerCase(), offlineData.splice(offlineData.indexOf(data.userName),1)[0]);
-			console.log("after splicing "+offlineData[data.userName]);
+			console.log(socket_log+"after splicing "+offlineData[data.userName]);
 		}
 		socket.broadcast.emit('newUser', users);
 		socket.emit('newUser', users);
